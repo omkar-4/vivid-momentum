5a1cfed02a51ea82d6bade5b516c45d07facb9ad
omkar-4 <136555960+omkar-4@users.noreply.github.com>
2026-06-02 01:51:27 +0530

    feat: launching early-alpha MVP

--- resources/platform/updater.js
+++ resources/platform/updater.js
@@ -1,11 +1,18 @@
 async function updater() {
   try {
+    if (typeof Neutralino === 'undefined' || !Neutralino.updater) {
+      console.error("Neutralino updater module is not loaded. Check permissions config.");
+      return;
+    }
+
     let url = "https://raw.githubusercontent.com/omkar-4/vivid-momentum/main/update_manifest.json";
-    let manifest = await Neutralino.updator.checkForUpdates(url);
+    let manifest = await Neutralino.updater.checkForUpdates(url);
 
     if (manifest.version != NL_APPVERSION) {
       await Neutralino.updator.install();
       await Neutralino.app.restartProcess();
+    } else {
+      console.log("You are using the latest version!");
     }
   } catch (err) {
     console.error("Update Error", err);
